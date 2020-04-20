b519883b9a5ae6ba5668cbad3ddafc27f446faae
Krista Rutz <rutzkri000@gmail.com>
2020-04-20 15:48:39 -0700

    Add prop type for eventual dynamic rendering of suggested profiles

--- src/components/pagebody/suggestionbar/ProfilePreview.js
+++ src/components/pagebody/suggestionbar/ProfilePreview.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ProfilePreview() {
-  return <React.Fragment></React.Fragment>;
+function ProfilePreview(props) {
+  return (
+    <React.Fragment>
+      <h4>@{props.username}</h4>
+      <p>{props.recentTweet}</p>
+      <hr />
+    </React.Fragment>
+  );
 }
+
+ProfilePreview.propTypes = {
+  username: PropTypes.string,
+  recentTweet: PropTypes.string,
+};
 
 export default ProfilePreview;
--- src/components/pagebody/suggestionbar/SuggestionColumn.js
+++ src/components/pagebody/suggestionbar/SuggestionColumn.js
@@ -1,7 +1,36 @@
 import React from "react";
+import ProfilePreview from "./ProfilePreview";
+
+const suggestedProfiles = [
+  {
+    username: "rando",
+    recentTweet: "we deserve better shipping",
+  },
+  {
+    username: "sumGuy",
+    recentTweet: "the world is blue",
+  },
+  {
+    username: "sumWoman",
+    recentTweet: "the trumpet is a brass instrument",
+  },
+];
 
 function SuggestionColumn() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <div>
+        <h5>Suggested for you</h5>
+        <ProfilePreview
+          // username={profilePreview.username}
+          // recentTweet={profilePreview.recentTweet}
+          username="test"
+          recentTweet="foo bar"
+        />
+        {/* map() to loop through suggestedProfiles */}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default SuggestionColumn;

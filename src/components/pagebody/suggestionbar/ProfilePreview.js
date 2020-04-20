4198742e4a244786c3f0a886ef658b1f85d0b9ac
Krista Rutz <rutzkri000@gmail.com>
2020-04-20 16:16:39 -0700

    Add icons to tweets

--- src/components/pagebody/suggestionbar/ProfilePreview.js
+++ src/components/pagebody/suggestionbar/ProfilePreview.js
@@ -4,7 +4,21 @@ import PropTypes from "prop-types";
 function ProfilePreview(props) {
   return (
     <React.Fragment>
-      <h4>@{props.username}</h4>
+      <div class="row">
+        <div class="col-xs-2">
+          <img
+            alt="twitter egg"
+            height="30"
+            border-radius="5%"
+            src="https://img.buzzfeed.com/buzzfeed-static/static/2017-03/31/13/enhanced/buzzfeed-prod-fastlane-02/original-grid-image-14740-1490981786-4.jpg?crop=590:590;5,0"
+          />
+        </div>
+        <div class="col-xs-10">
+          {" "}
+          <h4>@{props.username}</h4>
+        </div>
+      </div>
+
       <p>{props.recentTweet}</p>
       <hr />
     </React.Fragment>
--- src/components/pagebody/timeline/Tweet.js
+++ src/components/pagebody/timeline/Tweet.js
@@ -5,11 +5,23 @@ function Tweet(props) {
   return (
     <React.Fragment>
       <br />
-      <h3>@{props.username}</h3>
-      <p>
-        {props.content} #sofun #timeisanillusion #spiritualAF #alignyochakra
-      </p>
-      <h5>{props.timestamp}</h5>
+      <div class="row">
+        <div class="col-xs-2">
+          <img
+            alt="twitter egg"
+            height="45"
+            border-radius="5%"
+            src="https://img.buzzfeed.com/buzzfeed-static/static/2017-03/31/13/enhanced/buzzfeed-prod-fastlane-02/original-grid-image-14740-1490981786-4.jpg?crop=590:590;5,0"
+          />
+        </div>
+        <div class="col col-xs-10">
+          <h3>@{props.username}</h3>
+          <p>
+            {props.content} #sofun #timeisanillusion #spiritualAF #alignyochakra
+          </p>
+          <h5>{props.timestamp}</h5>
+        </div>
+      </div>
       <hr />
     </React.Fragment>
   );
